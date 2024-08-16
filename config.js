1608f64809419364ffbe11ded2c4deeba0e71b39
SADIYA OFC <118267877+sadiya44@users.noreply.github.com>
2024-08-16 17:41:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "put session id",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3cef367ebfc7a45c10595.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "SADIYA-MD"
 };
