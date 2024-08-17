c561cf0b9927e5e0789b3029e1614bd6e060d2b9
sanjuwabn4 <sanjuwaofc303@gmail.com>
2024-08-17 16:36:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put session id",
+SESSION_ID: process.env.SESSION_ID || "XZJ3xRAL#v3tHgP-hgDqQb7OfQSCo_U6CEGqm83rX3E3KZ_y1Pjo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3cef367ebfc7a45c10595.jpg"
 };
