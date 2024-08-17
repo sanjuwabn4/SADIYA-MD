071b39ce9990653eb5a40ec9e7299b3dba11657e
SADIYA OFC <118267877+sadiya44@users.noreply.github.com>
2024-08-17 14:53:41 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -9,7 +9,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: *🚀 Hi SADIYA-MD Is Online Now*\n\n*🚯 Owner* - Sadiya Ofc\n\n*🚯 Owner Number* -94742195461\n\n*🚯 Whatsapp Channel* - https://whatsapp.com/channel/0029VagR9a11iUxd0hUsD501\n\n_විධාන මෙනුව ලබා ගැනීමට *.menu* ලෙස ටයිප් කරන්න._},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
